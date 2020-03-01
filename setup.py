6473b4f82d87c0052ebef26eb712b8e13e2567da
Jason Cheatham <j.cheatham@gmail.com>
2020-03-01 14:05:02 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as rm:
 
 setuptools.setup(
     name="hubitatmaker",
-    version="0.3.0",
+    version="0.3.1",
     author="Jason Cheatham",
     author_email="j.cheatham@gmail.com",
     description="A library for interfacing with Hubitat via its Maker API",
