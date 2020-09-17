19d7ea2c8bb6cf1374594c05e0a1f8b09e8c2ee7
Jason Cheatham <jason@jasoncheatham.com>
2020-09-17 19:44:41 -0400

    Update version for publishing

--- hubitatmaker/__init__.py
+++ hubitatmaker/__init__.py
@@ -126,7 +126,7 @@ from .error import ConnectionError, InvalidConfig, InvalidToken, RequestError
 from .hub import Hub
 from .types import Attribute, Device, Event
 
-__version__ = "0.3.10"
+__version__ = "0.3.11"
 
 __all__ = [
     "ATTR_ACCELERATION",
