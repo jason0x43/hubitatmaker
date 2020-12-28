af54ffda488e7b80448328c0d65647845b78548f
Jason Cheatham <jason@jasoncheatham.com>
2020-12-28 11:22:39 -0500

    Bump version number

--- hubitatmaker/__init__.py
+++ hubitatmaker/__init__.py
@@ -134,7 +134,7 @@ from .error import ConnectionError, InvalidConfig, InvalidToken, RequestError
 from .hub import Hub
 from .types import Attribute, Device, Event
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 __all__ = [
     "ATTR_ACCELERATION",
